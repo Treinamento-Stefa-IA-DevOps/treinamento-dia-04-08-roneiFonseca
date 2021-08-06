eb83109567625fe0eb7f052394aa82a9fe861f32
Ronei <ronei.del.fonseca@gmail.com>
2021-08-06 11:30:22 -0300

    Completed exercise

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -4,9 +4,21 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+def titanic(Sex:int, Age:float, Lifeboat: int,Pclass:int):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
+
+    resp = titanic.predict([[Sex, Age, Lifeboat, Pclass]])    
+
+    if( resp):
+        return  {"survived": True,	"status": 200,	"message": "The passanger has lived." }
+    else: 
+        return {"survived": False,	"status": 200,	"message": "The passanger has died." }
+    
+        
+
+       
+        
 
 @app.get('/model')
 def get():
